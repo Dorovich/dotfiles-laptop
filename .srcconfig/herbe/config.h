27543d6a86340339845c148c05545ffb301d1f97
Dorovich <silverdovi25@gmail.com>
2023-01-05 02:59:07 +0100

    movimiento a .srcconfig

---
+++ .srcconfig/herbe/config.h
@@ -0,0 +1,19 @@
+static const char *background_color = "#191724";
+static const char *border_color = "#eb6f92";
+static const char *font_color = "#e0def4";
+static const char *font_pattern = "mononoki:size=12";
+static const unsigned line_spacing = 5;
+static const unsigned int padding = 15;
+
+static const unsigned int width = 300;
+static const unsigned int border_size = 2;
+static const unsigned int pos_x = 30;
+static const unsigned int pos_y = 30;
+
+enum corners { TOP_LEFT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_RIGHT };
+enum corners corner = TOP_RIGHT;
+
+static const unsigned int duration = 5; /* in seconds */
+
+#define DISMISS_BUTTON Button1
+#define ACTION_BUTTON Button3
